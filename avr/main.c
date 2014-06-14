1e687e4f6d27f1cfccd7099bbb80497e1a7cd435
Josef Gajdusek <atx@atalax.net>
2014-06-14 23:44:44 +0200

    Reworked the AVR I2C GPIO interface. Now we have atomic access to GPIO pins. Also this greatly simplifies driver code.

--- avr/main.c
+++ avr/main.c
@@ -1,10 +1,29 @@
-#include <avr/interrupt.h>
+	#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
 
-#define FW_VERSION		0x01
+#define FW_VERSION		0x02
 #define TWI_ADDRESS		0x32
+#define TWI_GPIO_BASE	0x00
+#define TWI_GPIO0		TWI_GPIO_BASE
+#define TWI_GPIO1		TWI_GPIO_BASE + 1
+#define TWI_GPIO2		TWI_GPIO_BASE + 2
+#define TWI_GPIO3		TWI_GPIO_BASE + 3
+#define TWI_GPIO4		TWI_GPIO_BASE + 4
+#define TWI_GPIO5		TWI_GPIO_BASE + 5
+#define TWI_GPIO6		TWI_GPIO_BASE + 6
+#define TWI_GPIO7		TWI_GPIO_BASE + 7
+#define TWI_GPIO8		TWI_GPIO_BASE + 8
+#define TWI_GPIO9		TWI_GPIO_BASE + 9
+#define TWI_LED0A		0x10
+#define TWI_LED0B		0x11
+#define TWI_LED2A		0x12
+#define TWI_LED2B		0x13
+#define TWI_MAGIC0		0x14
+#define TWI_MAGIC1		0x15
+#define TWI_MAGIC2		0x16
+#define TWI_VER			0x17
 
 #define TWS_RX_START	0x60
 #define TWS_RX_ACK		0x80
@@ -16,7 +35,14 @@
 #define TWS_TX_NACK		0xc0
 #define TWS_TX_END		0xc8
 
-#define GPIO_REG_NAME(a, r) a <= 0x01 ? &DDR##r : (a <= 0x03) ? &PIN##r : &PORT##r
+#define ASSEMBLE_GPIO(bank, bit) \
+	(!!(DDR##bank & _BV(bit))) | \
+	(!!(PORT##bank & _BV(bit)) << 1) | \
+	(!!(PIN##bank & _BV(bit)) << 2)
+
+#define DISASSEMBLE_GPIO(bank, bit, value) \
+	if(!!(DDR##bank & _BV(bit)) != (value & _BV(0))) DDR##bank ^= _BV(bit); \
+	if(!!(PORT##bank & _BV(bit)) != !!(value & _BV(1))) PORT##bank ^= _BV(bit);
 
 int main() {
 	TWAR = TWI_ADDRESS << 1;
@@ -37,89 +63,113 @@ int main() {
 }
 
 uint8_t get_address(uint8_t addr) {
-	if(addr <= 0x05) { /* GPIO */
-		volatile uint8_t *regb = GPIO_REG_NAME(addr, B);
-		volatile uint8_t *regc = GPIO_REG_NAME(addr, C);
-		volatile uint8_t *regd = GPIO_REG_NAME(addr, D);
-		if(addr & 1) { /* G7 - G0 */
-			return (!!(*regb & _BV(0)) << 2) | (!!(*regb & _BV(1)) << 1) | (!!(*regb & _BV(2)))
-					| (!!(*regc & _BV(1)) << 4) | (!!(*regc & _BV(2)) << 6) | (!!(*regc & _BV(3)) << 5)
-					| (!!(*regd & _BV(2)) << 7) | (!!(*regd & _BV(7)) << 3);
-		}
-		else {
-			return ((*regc & 1) << 1) | !!(*regd & _BV(4));
-		}
-	}
-	else {
-		switch(addr) {
-		case 0x06:
-			return OCR0B;
-		case 0x07:
-			return OCR0A;
-		case 0x08:
-			return OCR2A;
-		case 0x09:
-			return OCR2B;
-		case 0x10: /* Magic values */
-			return 'A';
-		case 0x11:
-			return 'T';
-		case 0x12:
-			return 'X';
-		case 0x13:
-			return FW_VERSION;
-		}
+	switch(addr) {
+	/* GPIO (00000<PIN><PORT><DDR>) */
+	case TWI_GPIO0:
+		return ASSEMBLE_GPIO(B, 2);
+	case TWI_GPIO1:
+		return ASSEMBLE_GPIO(B, 1);
+	case TWI_GPIO2:
+		return ASSEMBLE_GPIO(B, 0);
+	case TWI_GPIO3:
+		return ASSEMBLE_GPIO(D, 7);
+	case TWI_GPIO4:
+		return ASSEMBLE_GPIO(C, 1);
+	case TWI_GPIO5:
+		return ASSEMBLE_GPIO(C, 3);
+	case TWI_GPIO6:
+		return ASSEMBLE_GPIO(C, 2);
+	case TWI_GPIO7:
+		return ASSEMBLE_GPIO(D, 2);
+	case TWI_GPIO8:
+		return ASSEMBLE_GPIO(D, 4);
+	case TWI_GPIO9:
+		return ASSEMBLE_GPIO(C, 0);
+	/* LEDs */
+	case TWI_LED0B:
+		return OCR0B;
+	case TWI_LED0A:
+		return OCR0A;
+	case TWI_LED2A:
+		return OCR2A;
+	case TWI_LED2B:
+		return OCR2B;
+	/* Magic values */
+	case TWI_MAGIC0:
+		return 'A';
+	case TWI_MAGIC1:
+		return 'T';
+	case TWI_MAGIC2:
+		return 'X';
+	case TWI_VER:
+		return FW_VERSION;
 	}
 	return 0;
 }
 
 
 void set_address(uint8_t addr, uint8_t val) {
-	if(addr <= 0x05) { /* GPIO */
-		volatile uint8_t *regb = GPIO_REG_NAME(addr, B);
-		volatile uint8_t *regc = GPIO_REG_NAME(addr, C);
-		volatile uint8_t *regd = GPIO_REG_NAME(addr, D);
-		if(addr & 1) { /* G7 - G0 */
-			*regb = (*regb & ~0b00000111) | (!!(val & _BV(2))) | (!!(val & _BV(1)) << 1) | (!!(val & _BV(0)) << 2);
-			*regc = (*regc & ~0b00001110) | (!!(val & _BV(4)) << 1) | (!!(val & _BV(6)) << 2) | (!!(val & _BV(5)) << 3);
-			*regd = (*regd & ~0b10000100) | (!!(val & _BV(7)) << 2) | (!!(val & _BV(3)) << 7);
-		}
-		else { /* G9, G8 */
-			*regc = (*regc & ~1) | !!(val & 2);
-			*regd = (*regd & ~_BV(4)) | ((val & 1) << 4);
-		}
-	}
-	else { /* LEDs */
-		switch(addr) {
-		case 0x06: /* 0B */
-			if(!val)
-				TCCR0A &= ~(_BV(COM0B1));
-			else
-				TCCR0A |= _BV(COM0B1);
-			OCR0B = val;
-			break;
-		case 0x07: /* 0A*/
-			if(!val)
-				TCCR0A &= ~(_BV(COM0A1));
-			else
-				TCCR0A |= _BV(COM0A1);
-			OCR0A = val;
-			break;
-		case 0x08: /* 2A */
-			if(!val)
-				TCCR2A &= ~(_BV(COM2A1));
-			else
-				TCCR2A |= _BV(COM2A1);
-			OCR2A = val;
-			break;
-		case 0x09: /* 2B */
-			if(!val)
-				TCCR2A &= ~(_BV(COM2B1));
-			else
-				TCCR2A |= _BV(COM2B1);
-			OCR2B = val;
-			break;
-		}
+	switch(addr) {
+	/* GPIO */
+	case TWI_GPIO0:
+		DISASSEMBLE_GPIO(B, 2, val);
+		break;
+	case TWI_GPIO1:
+		DISASSEMBLE_GPIO(B, 1, val);
+		break;
+	case TWI_GPIO2:
+		DISASSEMBLE_GPIO(B, 0, val);
+		break;
+	case TWI_GPIO3:
+		DISASSEMBLE_GPIO(D, 7, val);
+		break;
+	case TWI_GPIO4:
+		DISASSEMBLE_GPIO(C, 1, val);
+		break;
+	case TWI_GPIO5:
+		DISASSEMBLE_GPIO(C, 3, val);
+		break;
+	case TWI_GPIO6:
+		DISASSEMBLE_GPIO(C, 2, val);
+		break;
+	case TWI_GPIO7:
+		DISASSEMBLE_GPIO(D, 2, val);
+		break;
+	case TWI_GPIO8:
+		DISASSEMBLE_GPIO(D, 4, val);
+		break;
+	case TWI_GPIO9:
+		DISASSEMBLE_GPIO(C, 0, val);
+		break;
+	/* LEDs */
+	case TWI_LED0B:
+		if(!val)
+			TCCR0A &= ~(_BV(COM0B1));
+		else
+			TCCR0A |= _BV(COM0B1);
+		OCR0B = val;
+		break;
+	case TWI_LED0A:
+		if(!val)
+			TCCR0A &= ~(_BV(COM0A1));
+		else
+			TCCR0A |= _BV(COM0A1);
+		OCR0A = val;
+		break;
+	case TWI_LED2A:
+		if(!val)
+			TCCR2A &= ~(_BV(COM2A1));
+		else
+			TCCR2A |= _BV(COM2A1);
+		OCR2A = val;
+		break;
+	case TWI_LED2B:
+		if(!val)
+			TCCR2A &= ~(_BV(COM2B1));
+		else
+			TCCR2A |= _BV(COM2B1);
+		OCR2B = val;
+		break;
 	}
 }
 
